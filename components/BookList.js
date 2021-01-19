d9d0df9fa6eca8e15ed8685361300e12a7293e47
Thomas J. Fox <thomasjfox1@gmail.com>
2021-01-18 22:45:40 -0600

    fix

--- components/BookList.js
+++ components/BookList.js
@@ -1,7 +1,7 @@
 import Image from 'next/image';
 import Link from 'next/link';
 
-import styles from './Booklist.module.scss';
+import styles from './BookList.module.scss';
 
 const BookList = ({ books }) => {
   return (
