a4ad1ba8c8d9e264d9db347c0436028903a65bca
Thomas J. Fox <thomasjfox1@gmail.com>
2021-02-23 21:57:04 -0600

    fixes

--- utils/mdPosts.js
+++ utils/mdPosts.js
@@ -1,10 +1,10 @@
-import fs from 'fs'
-import path from 'path'
-import matter from 'gray-matter'
-import remark from 'remark'
-import html from 'remark-html'
+import fs from 'fs';
+import path from 'path';
+import matter from 'gray-matter';
+import remark from 'remark';
+import html from 'remark-html';
 
-const postsDirectory = path.join(process.cwd(), 'reads')
+const postsDirectory = path.join(process.cwd(), 'reads');
 
 export function getSortedPostsData() {
   // Get file names under /posts
@@ -37,7 +37,7 @@ export function getSortedPostsData() {
 }
 
 export function getAllPostIds() {
-  const fileNames = fs.readdirSync(postsDirectory)
+  const fileNames = fs.readdirSync(postsDirectory);
 
   // Returns an array that looks like this:
   // [
@@ -57,27 +57,27 @@ export function getAllPostIds() {
       params: {
         id: fileName.replace(/\.md$/, '')
       }
-    }
-  })
+    };
+  });
 }
 
 export async function getPostData(id) {
-  const fullPath = path.join(postsDirectory, `${id}.md`)
-  const fileContents = fs.readFileSync(fullPath, 'utf8')
+  const fullPath = path.join(postsDirectory, `${id}.md`);
+  const fileContents = fs.readFileSync(fullPath, 'utf8');
 
   // Use gray-matter to parse the post metadata section
-  const matterResult = matter(fileContents)
+  const matterResult = matter(fileContents);
 
   // Use remark to convert markdown into HTML string
   const processedContent = await remark()
     .use(html)
-    .process(matterResult.content)
-  const contentHtml = processedContent.toString()
+    .process(matterResult.content);
+  const contentHtml = processedContent.toString();
 
   // Combine the data with the id and contentHtml
   return {
     id,
     contentHtml,
     ...matterResult.data
-  }
+  };
 }
